a552a64b686f131ee496de64e55aa7289597db66
Claire Mitchell <claireinez@users.noreply.github.com>
2020-11-27 18:31:34 +0000

    add TODO messages

--- src/utils/calculateRewardPerHour.js
+++ src/utils/calculateRewardPerHour.js
@@ -1,3 +1,6 @@
 export default function calculateRewardPerHour(reward, time) {
+  // TODO: calculate reward per hour
+  // reward: £ per person
+  // time: length of study (in minutes)
   return 1;
 }
--- src/utils/calculateTotalCost.js
+++ src/utils/calculateTotalCost.js
@@ -1,11 +1,12 @@
 export default function calculateTotalCost(
-  places,
-  reward,
+  numberOfParticipants,
+  rewardPerParticipant,
   feesPercentage,
   vatPercentage
 ) {
+  // TODO: calculate total cost for the study
   // Fees are charged as a percentage of the cost for all places
   // VAT is charged as a percentage of the fees
   // Total cost is the base cost plus fees plus VAT
-  return places * reward;
+  return 1;
 }
