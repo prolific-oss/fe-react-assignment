420bcdf73652e54e6d8031cf97ad5708ece0309b
Leo Reeves <leojreeves@gmail.com>
2021-08-19 17:49:53 +0100

    refactor: restore zeroes

--- src/utils/calculateTotalCost.test.js
+++ src/utils/calculateTotalCost.test.js
@@ -8,12 +8,12 @@ test('correctly calculates totalCost given participant and reward inputs', () =>
       feesPercentage: 0.3,
       vatPercentage: 0.2,
     })
-  ).toEqual(136000.0);
+  ).toEqual(136000.00);
   expect(
     calculateTotalCost({ numberOfParticipants: 1, rewardPerParticipant: 1, feesPercentage: 0.3, vatPercentage: 0.2 })
   ).toEqual(1.36);
 });
 
 test('totalCost is 0 when participants or reward are 0', () => {
-  expect(calculateTotalCost({ numberOfParticipants: 0, reward: 0 })).toEqual(0.0);
+  expect(calculateTotalCost({ numberOfParticipants: 0, reward: 0 })).toEqual(0.00);
 });
