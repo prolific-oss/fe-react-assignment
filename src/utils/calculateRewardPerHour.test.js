84f185c2d9e254ce70f3f3708800022fe0d765df
Mantas Astrauskas <mastrauskas4@gmail.com>
2021-08-25 12:31:20 +0100

    Update src/utils/calculateRewardPerHour.test.js
    Co-authored-by: Leo Reeves <leojreeves@gmail.com>

--- src/utils/calculateRewardPerHour.test.js
+++ src/utils/calculateRewardPerHour.test.js
@@ -5,7 +5,7 @@ test('correctly calculates rewardPerHour given timeInMinutes and rewardPerPartic
   expect(calculateRewardPerHour({ rewardPerParticipant: 100, timeInMinutes: 60 })).toEqual(100.00);
 });
 
-test('rewardPerHour is 0 when timeInMinutes or rewardPerParticipant are 0', () => {
+test('rewardPerHour is 0 when timeInMinutes or rewardPerParticipant is 0', () => {
   expect(calculateRewardPerHour({ rewardPerParticipant: 0, timeInMinutes: 0 })).toEqual(0.00);
   expect(calculateRewardPerHour({ rewardPerParticipant: 10, timeInMinutes: 0 })).toEqual(0.00);
   expect(calculateRewardPerHour({ rewardPerParticipant: 0, timeInMinutes: 10 })).toEqual(0.00);
